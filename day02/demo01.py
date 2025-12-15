96ff7b46977b4d26c165311f57579f22b3a6e9dc
Parth2199 <parthkoik013@gmail,com>
2025-12-15 19:24:50 +0530

    assig 2nd

---
+++ day02/demo01.py
@@ -0,0 +1,17 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key=os.getenv("api")
+
+flag='yes'if api_key else 'no'
+print("Api key Loaded !!")
+
+city=input("Enter City : ")
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+response=requests.get(url)
+weather=response.json()
+#print("Weather : ",weather )
+print("Temperature: ", weather["main"]["temp"])
