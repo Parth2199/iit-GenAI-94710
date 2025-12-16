85a449797e1c878f6f3bccc3e73ed4abc7ad9f30
Parth2199 <parthkoik013@gmail,com>
2025-12-16 12:02:16 +0530

    Assig 4

---
+++ day04/demo01.py
@@ -0,0 +1,33 @@
+import streamlit as st 
+import time 
+
+st.set_page_config(page_title="ChatBot")
+st.title(" Chatbot UI ")
+
+with st.sidebar:
+    st.header("Setting")
+
+if "message" not in st.session_state:
+    st.session_state.message=[]
+
+for msg in st.session_state.message:
+    with st.chat_message(msg["role"]):
+        st.write(msg["content"])
+
+def response(text):
+    for word in text.split():
+        yield word + " "
+        time.sleep(0.2)
+
+chat_ip = st.chat_input("Enter Message")
+
+if chat_ip:
+    st.session_state.message.append({"role": "user", "content": chat_ip})
+    with st.chat_message("user"):
+        st.write(chat_ip)
+
+    bot_reply = f"You said : {chat_ip}"
+
+    st.session_state.message.append({"role": "user", "content": chat_ip})
+    with st.chat_message("assistant"):
+        st.write_stream(response(bot_reply))
